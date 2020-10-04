2d9ba715798d8724fd5813d9f3a3ecc74d3306cb
Fil_Kirill <kirill.filobodchenko@innroad.com>
2020-10-04 23:45:11 +0300

    Changed Connection String

--- DemosTask/Startup.cs
+++ DemosTask/Startup.cs
@@ -23,12 +23,13 @@ namespace DemosTask
         {
             services.AddControllersWithViews();
 
-            services.AddDbContext<CinemaContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+            services.AddDbContext<CinemaContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddSwaggerGen(swagger =>
             {
                 swagger.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
