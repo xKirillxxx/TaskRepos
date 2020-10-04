6c9a6c542731179f28bb21efcb1fbf08f5502bf4
Fil_Kirill <kirill.filobodchenko@innroad.com>
2020-10-04 20:31:59 +0300

    Changed Tests

--- DemoTask.Tests/UnitTestController.cs
+++ DemoTask.Tests/UnitTestController.cs
@@ -11,29 +11,35 @@ namespace DemoTask.Tests
         private readonly CinemaContext context;
 
         [TestMethod]
-        public async void TestMethod1() //
+        public async void GetTest() //
         {
             // Arrange
             var controller = new CinemasController(context);
 
-            var cinema_post = new Cinema { Name = "Vivas", Rating = 7, Price = 10, Time = 100 };
-            var cinema_put = new Cinema { Name = "Vi", Rating = 71, Price = 110, Time = 1100 };
-
             // Act
             var resultGET = await controller.GetCinema(3);
-            var resultDelete = await controller.DeleteCinema(1);
-            var resultCreate = await controller.PostCinema(cinema_post);
-            var resultEdit = await controller.PutCinema( 3 , cinema_put);
 
             // Assert
-            Assert.IsNull(resultDelete);
-            Assert.IsNotNull(resultCreate);
-            Assert.AreNotEqual(resultGET, resultEdit);
             Assert.IsNotNull(resultGET);
         }
 
         [TestMethod]
-        public async void TestMethod2()
+        public async void PostMethod()
+        {
+            // Arrange
+            var controller = new CinemasController(context);
+
+            var cinema_post = new Cinema { Name = "Vivas", Rating = 7, Price = 10, Time = 100 };
+
+            // Act
+            var resultCreate = await controller.PostCinema(cinema_post);
+
+            // Assert
+            Assert.IsNotNull(resultCreate);
+        }
+
+        [TestMethod]
+        public async void PutMethod()
         {
             // Arrange
             var controller = new CinemasController(context);
@@ -43,15 +49,27 @@ namespace DemoTask.Tests
 
             // Act
             var resultGET = await controller.GetCinema(3);
-            var resultDelete = await controller.DeleteCinema(1);
-            var resultCreate = await controller.PostCinema(cinema_post);
             var resultEdit = await controller.PutCinema(3, cinema_put);
 
             // Assert
-            Assert.IsNull(resultDelete);
-            Assert.IsNotNull(resultCreate);
             Assert.AreNotEqual(resultGET, resultEdit);
-            Assert.IsNotNull(resultGET);
+        }
+
+        [TestMethod]
+        public async void EditMethod()
+        {
+            // Arrange
+            var controller = new CinemasController(context);
+
+            var cinema_post = new Cinema { Name = "Vivas", Rating = 7, Price = 10, Time = 100 };
+            var cinema_put = new Cinema { Name = "Vi", Rating = 71, Price = 110, Time = 1100 };
+
+            // Act
+            var resultGET = await controller.GetCinema(3);
+            var resultEdit = await controller.PutCinema(3, cinema_put);
+
+            // Assert
+            Assert.AreNotEqual(resultGET, resultEdit);
         }
     }
 }
